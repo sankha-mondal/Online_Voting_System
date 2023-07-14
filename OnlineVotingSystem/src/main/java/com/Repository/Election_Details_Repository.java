343a42bd3fca012bc51effc98421671fe84a4418
Sankha Subhra Mondal <sankha123@gmail.com>
2023-07-14 10:41:39 +0530

    Online Voting System [Backend: Spring-Boot, Frontend: JSP, Database: MySQL]

--- OnlineVotingSystem/src/main/java/com/Repository/Election_Details_Repository.java
+++
@@ -1,9 +0,0 @@
-package com.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Entity.Election_Details;
-
-public interface Election_Details_Repository  extends JpaRepository<Election_Details,Integer>{
-    
-}
--- OnlineVotingSystem/src/main/java/com/Repository/Party_Details_Repository.java
+++
@@ -1,9 +0,0 @@
-package com.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Entity.Party_Details;
-
-public interface Party_Details_Repository  extends JpaRepository<Party_Details,String>{
-    
-}
